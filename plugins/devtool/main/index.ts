3c7ef6c0ccd6170bfab44310502b9423d6fd5515
Swain <7621572+talisk@users.noreply.github.com>
2026-01-04 15:07:32 +0800

    opt: code optimize

--- plugins/devtool/main/index.ts
+++ plugins/devtool/main/index.ts
@@ -3,43 +3,8 @@
 // LICENSE file in the root directory of this source tree.
 
 import { definePlugin, MainContext } from '@lynx-js/devtool-plugin-core/main';
-import axios from 'axios';
-import { compareVersions, validate } from 'compare-versions';
 
-let _params: any, cliVersion: string | undefined;
-
-const semverCompare = (a: string, b: string): number => {
-  if (validate(a) && validate(b)) {
-    return compareVersions(a, b);
-  }
-  // If a or b doesn't conform to semantic version format, fallback to use localeCompare to prevent code errors
-  return a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' });
-};
-
-const getCliVersion = async (host: string) => {
-  if (cliVersion) {
-    return Promise.resolve(cliVersion);
-  }
-  try {
-    const rt = await axios.get(`http://${host}/getVersion`);
-    cliVersion = rt.data?.data;
-    return cliVersion;
-  } catch (_) {
-    return undefined;
-  }
-};
-
-const isLDT3 = async (host: string) => {
-  try {
-    const version = await getCliVersion(host);
-    if (!version) {
-      return true;
-    }
-    return semverCompare(version, '0.0.64') > 0;
-  } catch (_) {
-    return true;
-  }
-};
+let _params: any;
 
 const bridge = (_: MainContext) => ({
   getInspectUrl: async (type?: 'web' | 'lynx') => {
@@ -61,7 +26,7 @@ const bridge = (_: MainContext) => ({
           return undefined;
         }
       }
-      const res = (await isLDT3(host)) ? 'devtools_app' : 'inspector';
+      const res = 'inspector';
       return `http://${host}/localResource/devtool/${viewMode}/${res}.html`;
     } catch (error) {
       console.error('Failed to construct inspect URL:', error);
